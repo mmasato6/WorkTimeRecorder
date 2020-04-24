abd77db70aad9d4c0164bc447ec0ee69a6b6ea22
mutsuya masato <mutsuya_masato@yahoo.co.jp>
2020-04-24 09:11:04 +0900

    first commit

---
+++ WorkTimeRecorder.bat
@@ -0,0 +1,18 @@
+@ECHO OFF
+setlocal
+
+REM このバッチファイルと同じ階層にPowerShellスクリプトファイルが置かれている前提
+set mydir=%~dp0
+set pspath=%mydir%WorkTimeRecoeder.ps1
+rem 指定されたps1を実行する
+REM set pspath=%1
+
+REM 
+REM -ExecutionPolicy 一時的に実行ポリシーを緩和する
+REM https://qiita.com/tomoko523/items/df8e384d32a377381ef9
+powershell -NoProfile -ExecutionPolicy RemoteSigned %pspath%
+
+REM 終了
+REM PAUSE
+endlocal
+rem exit
